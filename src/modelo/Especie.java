905e364ac25d161e1b5c648abcd6b936f137b3f0
Braian Medrano <braiankpo_18@hotmail.com>
2024-04-07 21:45:32 -0300

    Se agrea el constructor a especie

--- src/modelo/Especie.java
+++ src/modelo/Especie.java
@@ -3,6 +3,12 @@ package modelo;
 public class Especie {
     private String nombre;
     private double porcentajeComida;
+
+    public Especie(String nombre, double porcentajeComida) {
+        this.nombre = nombre;
+        this.porcentajeComida = porcentajeComida;
+    }
+    
     
     // Setter
     public void setNombre(String nombre) {
