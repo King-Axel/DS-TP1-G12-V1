acff7c1a60c3647c853b50ef10819e2a91b1c7fe
Braian Medrano <braiankpo_18@hotmail.com>
2024-04-07 22:01:24 -0300

    Se agrega una clase de prueba y se agregan constructores

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,6 +1,11 @@
 package javaapplication1;
 
 
+import modelo.Animal;
+import modelo.Carnivoro;
+import modelo.Empleado;
+import modelo.Especie;
+import modelo.Pais;
 import modelo.Sector;
 import modelo.Sectores;
 import vista.Vista_Principal;
@@ -10,8 +15,28 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         Vista_Principal ventana = new Vista_Principal();
         
-        Sector sector1 = new Sector();
-        sector1.setNumero(0);
+        Pais pais1 = new Pais("Congo");
+        Pais pais2 = new Pais("Nigeria");
+        
+        Especie especie1 = new Especie("Tigre", 20);
+        Especie especie2 = new Especie("Leon", 20);
+
+//        Edad, peso, pais, especie
+        Carnivoro animal1 = new Carnivoro(10, 150, pais1 , especie1);
+        Carnivoro animal2 = new Carnivoro(10, 210, pais1 , especie2);
+                
+        
+//        Nombre, dni, domicilio
+        Empleado empleado1 = new Empleado("Patricio Reyes", 45321584, "Siempre viva 124");
+        Empleado empleado2 = new Empleado("Ramiro Pereyra", 36541247, "Avenida Rosario");
+        Empleado empleado3 = new Empleado("Ana Villanueva", 41521457, "Siempre viva 124");
+        
+//        Numero de sector, empleado, capacidad, latitud, longitud
+        Sector sector1 = new Sector(1, empleado1, 10, 1235417, 9214712 );
+        sector1.agregarAnimal(animal1);
+        sector1.agregarAnimal(animal2);
+        
+        
         
         
         Sectores grupoSectores = new Sectores();
--- src/modelo/Pais.java
+++ src/modelo/Pais.java
@@ -2,6 +2,12 @@ package modelo;
 
 public class Pais {
     private String nombre;
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+    
+  
     
     public void setNombre(String nombre) {
         this.nombre = nombre;
